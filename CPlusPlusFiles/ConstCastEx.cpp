5314c3254f13036f73318aa91ab4ba0e64a3772a
Nitesh Kartha <karthanitesh@gmail.com>
2016-12-24 13:20:37 -0600

    Added Files from 12/24/16, last original commit for a while

---
+++ CPlusPlusFiles/ConstCastEx.cpp
@@ -0,0 +1,16 @@
+//const_cast
+
+#include <iostream>
+using namespace std;
+
+void print(char * str){
+	cout << str << endl;
+}
+
+int main() {
+
+	const char * c = "sample text";
+	print(const_cast<char *> (c));
+	return 0;
+	
+}
